8aabdf3e2480333c9d07f849d5baec2a0c75c54b
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2026-05-23 17:26:17 +0530

    Refactor query endpoint to use streaming response and simplify context handling

--- backend/app/api/query.py
+++ backend/app/api/query.py
@@ -1,46 +1,24 @@
 from fastapi import APIRouter
+from fastapi.responses import StreamingResponse
 
 from backend.app.rag.retriever import retrieve_chunks
-from backend.app.rag.generator import generate_response
-from backend.app.rag.memory import (
-    save_message,
-    get_memory
-)
+from backend.app.rag.generator import stream_response
 
 router = APIRouter()
 
 @router.get("/query")
-async def query_rag(
-    q: str,
-    session_id: str = "default"
-):
+async def query_rag(q: str):
 
     results = retrieve_chunks(q)
 
     contexts = [row[0] for row in results]
 
-    memory = get_memory(session_id)
-
-    answer = generate_response(
+    generator = stream_response(
         query=q,
-        contexts=contexts,
-        memory=memory
+        contexts=contexts
     )
 
-    save_message(
-        session_id,
-        "user",
-        q
+    return StreamingResponse(
+        generator,
+        media_type="text/plain"
     )
-
-    save_message(
-        session_id,
-        "assistant",
-        answer
-    )
-
-    return {
-        "query": q,
-        "answer": answer,
-        "session_id": session_id
-    }
--- backend/app/rag/generator.py
+++ backend/app/rag/generator.py
@@ -1,37 +1,30 @@
 import ollama
 
-def generate_response(query, contexts, memory):
+def stream_response(query, contexts):
 
     context_text = "\n\n".join(contexts)
 
-    memory_text = ""
-
-    for role, msg in memory:
-        memory_text += f"{role}: {msg}\n"
-
     prompt = f"""
-You are a helpful AI assistant.
-
-Conversation History:
-{memory_text}
-
 Context:
 {context_text}
 
 Question:
 {query}
 
-Answer ONLY using context if relevant.
+Answer briefly.
 """
 
-    response = ollama.chat(
+    stream = ollama.chat(
         model="llama3",
         messages=[
             {
                 "role": "user",
                 "content": prompt
             }
-        ]
+        ],
+        stream=True
     )
 
-    return response["message"]["content"]
+    for chunk in stream:
+
+        yield chunk["message"]["content"]
